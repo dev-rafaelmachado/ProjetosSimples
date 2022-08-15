de2821191258b38a36f99b42d0f7f5aab2faf992
Rafael Machado <dev.rafaelmachado@gmail.com>
2022-08-15 15:55:35 -0300

    Event on Buttons

--- CalculadoraJS/main.js
+++ CalculadoraJS/main.js
@@ -3,6 +3,27 @@ function InserirNumber(number) {
     document.getElementById("dsp").appendChild(text)
 }
 
+function Clear(tp){
+    if (tp == 'C'){
+        let value = document.getElementById("dsp").innerHTML
+        value = value.slice(0, -1);
+        document.getElementById("dsp").innerHTML = value
+    } else { 
+        stats = false
+        document.getElementById("dsp").innerHTML = ""
+    }
+}
+
 function RealizarOper(Oper) {
-    
+    if (stats == false){
+        var stats = true
+        var opera1 = parseFloat(document.getElementById("dsp").innerHTML)
+        document.getElementById("dsp").innerHTML = ""
+    } else if (stats == true) {
+        var opera2 = parseFloat(document.getElementById("dsp").innerHTML)
+        if (Oper == '*') {
+            const result = opera1 * opera2
+            document.getElementById("dsp").innerHTML = ""
+        }
+    }
 }
