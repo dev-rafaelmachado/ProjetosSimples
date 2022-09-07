84f3c89beee87fc635dc9a77d4b48a4c13769f19
Rafael Machado <dev.rafaelmachado@gmail.com>
2022-09-06 23:29:28 -0300

    a

--- Pokedex/main.js
+++ Pokedex/main.js
@@ -2,17 +2,18 @@ const pokemonImg = document.querySelector('#imgPoke')
 const inptPoke = document.querySelector('#namePoke')
 const nomePoke = document.querySelector('#nomePoke')
 const IDpoke = document.querySelector('#IDpoke')
+const btprev =  document.querySelector('#prev')
+const btnext =  document.querySelector('#next')
 
+var idAtual = 1
 const fetchPoke = async (pokemon) =>{
     const APIresp = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const APIdata = await APIresp.json();
-    console.log(APIdata)
     return APIdata
 }
 
 const renderPoke = async (pokemon) => {
     const data = await fetchPoke(pokemon)
-    console.log(data)
     if (data.id > 649){
         pokemonImg.src = data['sprites']['front_default'] // ['versions']['generation-vi']['x-y']
         pokemonImg.style.width = "215px"
@@ -33,4 +34,21 @@ inptPoke.addEventListener('change', (event) => {
     renderPoke(inptPoke.value.toLowerCase())
 });
 
-renderPoke('1')
+btprev.addEventListener('click' , (event) => {
+    event.preventDefault();
+    if (idAtual > 1){
+        idAtual -= 1
+        renderPoke(idAtual)
+    }
+});
+
+btnext.addEventListener('click' , (event) => {
+    event.preventDefault();
+    
+    idAtual += 1
+    renderPoke(idAtual)
+
+});
+
+renderPoke(idAtual)
+
