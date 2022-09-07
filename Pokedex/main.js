4323fa4815f61b35a398f40f6c12ee3bf3e5522f
Rafael Machado <dev.rafaelmachado@gmail.com>
2022-09-06 23:00:01 -0300

    Outhers

--- Pokedex/main.js
+++ Pokedex/main.js
@@ -1,9 +1,12 @@
 const pokemonImg = document.querySelector('#imgPoke')
 const inptPoke = document.querySelector('#namePoke')
+const nomePoke = document.querySelector('#nomePoke')
+const IDpoke = document.querySelector('#IDpoke')
 
 const fetchPoke = async (pokemon) =>{
     const APIresp = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const APIdata = await APIresp.json();
+    console.log(APIdata)
     return APIdata
 }
 
@@ -12,17 +15,20 @@ const renderPoke = async (pokemon) => {
     console.log(data)
     if (data.id > 649){
         pokemonImg.src = data['sprites']['front_default'] // ['versions']['generation-vi']['x-y']
-        pokemonImg.style.width = "500px"
+        pokemonImg.style.width = "215px"
+        pokemonImg.style.height = "215px"
     } else {
         pokemonImg.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
-        pokemonImg.style.width = "500px"
+        pokemonImg.style.width = "100%"
+        pokemonImg.style.height = "100%"
     }
-    
-
+    const nome = data.name
+    nomePoke.innerHTML = nome.charAt(0).toUpperCase() + nome.slice(1);
+    IDpoke.innerHTML = "#"+ data.id + " -"
 
 }
 
-inptPoke.addEventListener('change', (event) =>{
+inptPoke.addEventListener('change', (event) => {
     event.preventDefault();
     renderPoke(inptPoke.value.toLowerCase())
 });
