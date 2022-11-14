404f1290691f078e306a5d7112e23145ea054c3f
Rafael Machado <dev.rafaelmachado@gmail.com>
2022-11-14 00:53:27 -0300

    Buttons conselhos

--- Conselhos/main.js
+++ Conselhos/main.js
@@ -1,15 +1,30 @@
-
+const reload_ELE = document.querySelector("#reload"),
+  copy_ELE = document.querySelector("#copy"),
+  text = document.querySelector("#element");
 
 const getAPI = async () => {
-    const responseAPI = await fetch("https://api.adviceslip.com/advice")
-    const APIdata = await responseAPI.json();
-    return await APIdata
-}
+  const responseAPI = await fetch("https://api.adviceslip.com/advice");
+  const APIdata = await responseAPI.json();
+  return await APIdata;
+};
 
-const renderAdvice = async () =>{
-    const data = await getAPI()
-    
-    document.querySelector("#element").textContent = data.slip.advice
-}
+const renderAdvice = async () => {
+  const data = await getAPI();
 
-renderAdvice()
+  text.textContent = data.slip.advice;
+};
+
+renderAdvice();
+
+// ---------------------- //
+
+reload_ELE.addEventListener("click", (event) => {
+  event.preventDefault();
+  renderAdvice();
+});
+
+copy_ELE.addEventListener("click", (event) => {
+  event.preventDefault();
+  navigator.clipboard.writeText(text.textContent);
+  copy_ELE.className = "animation "
+});
